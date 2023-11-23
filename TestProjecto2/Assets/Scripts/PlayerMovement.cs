db2e8358d71eaec6bef51deb3e10c0040eb32c70
juan [itb] cardador cruces <juan.cardador.7e4@itb.cat>
2023-11-23 17:49:32 +0100

    Unity Project added

---
+++ TestProjecto2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private PlayerInput _pinput;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private Vector2 _movementInput;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        _rb.velocity = _movementInput*_speed;
+
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>();
+        if (_movementInput.x != 0 || _movementInput.y !=0)
+        {
+            _animator.SetFloat("Horizontal", _movementInput.x);
+            _animator.SetFloat("Vertical", _movementInput.y);
+            _animator.SetBool("isMoving", true);        
+        }
+        else _animator.SetBool("isMoving", false);
+
+
+
+    }
+}
