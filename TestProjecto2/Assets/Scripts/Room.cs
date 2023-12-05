e227a2411866e81ce57941347f1ad93e1d9b4e88
AngelNavarreteSanchez <angel.navarrete.7e6@itb.cat>
2023-12-05 19:43:09 +0100

    Room Sprite Created

--- TestProjecto2/Assets/Scripts/Room.cs
+++ TestProjecto2/Assets/Scripts/Room.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject topDoor;
+    [SerializeField] GameObject bottonDoor;
+    [SerializeField] GameObject leftDoor;
+    [SerializeField] GameObject rightDoor;
+
+    public Vector2Int RoomIndex {  get; set; }
+    
+    public void OpenDoor(Vector2Int direction)
     {
-        
+        if (direction == Vector2Int.up)
+        {
+            topDoor.SetActive(true);
+        }
+        if (direction == Vector2Int.down)
+        {
+            bottonDoor.SetActive(true);
+        }
+        if(direction == Vector2Int.left)
+        {
+            leftDoor.SetActive(true);
+        }
+        if(direction == Vector2Int.right){
+            rightDoor.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- TestProjecto2/Assets/Scripts/RoomManager.cs
+++ TestProjecto2/Assets/Scripts/RoomManager.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject roomPrefab;
+    [SerializeField] private int maxRooms = 15;
+    [SerializeField] private int minRooms = 10;
+
+    int roomWidth = 18;
+    int roomHeight = 10;
+
+    int gridSizeX = 10;
+    int gridSizeY = 10;
+
+    private List<GameObject> rooms = new List<GameObject>();
+    private Queue<Vector2Int> roomQueue = new Queue<Vector2Int>();
+    private int[,] roomGrid;
+    private int roomCount;
+
+    private void Start()
     {
-        
+        roomGrid = new int[gridSizeX, gridSizeY];
+        roomQueue = new Queue<Vector2Int>();
+        Vector2Int initialRoomIndex = new Vector2Int(gridSizeX/2, gridSizeY/2);
+        RoomGenerator(initialRoomIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RoomGenerator(Vector2Int roomIndex)
     {
-        
+        roomQueue.Enqueue(roomIndex);
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+        roomGrid[x, y] = 1;
+        roomCount++;
+        var initialRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomIndex),Quaternion.identity);
+        initialRoom.name = $"Room-{roomCount}";
+        initialRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        rooms.Add(initialRoom);
+    }
+    private Vector3 GetPositionFromGridIndex(Vector2Int gridIndex)
+    {
+        int gridX = gridIndex.x;
+        int gridY = gridIndex.y;
+        return new Vector3(roomWidth * (gridX - gridSizeX / 2), roomHeight * (gridY - gridSizeY / 2));
+    }
+    private void OnDrawGizmos()
+    {
+        Color gizmoColor = new Color(0, 1, 1, 0.05f);
+        Gizmos.color = gizmoColor;
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 position = GetPositionFromGridIndex(new Vector2Int(x, y));
+                Gizmos.DrawWireCube(position, new Vector3(roomWidth,roomHeight,1));
+            }
+        }
     }
 }
