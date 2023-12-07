7946da44a13279e09da759cedfdcc077a4b05118
angel [itb] navarrete sanchez <angel.navarrete.7e6@itb.cat>
2023-12-07 17:16:04 +0100

    Procedular Map Created

--- TestProjecto2/Assets/Scripts/Room.cs
+++ TestProjecto2/Assets/Scripts/Room.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    [Header("Doors")]
     [SerializeField] GameObject topDoor;
     [SerializeField] GameObject bottonDoor;
     [SerializeField] GameObject leftDoor;
     [SerializeField] GameObject rightDoor;
+    [Header("Walls")]
+    [SerializeField] GameObject topWall;
+    [SerializeField] GameObject bottonWall;
+    [SerializeField] GameObject leftWall;
+    [SerializeField] GameObject rightWall;
 
     public Vector2Int RoomIndex {  get; set; }
     
     public void OpenDoor(Vector2Int direction)
     {
-        if (direction == Vector2Int.up) topDoor.SetActive(true);
+        if (direction == Vector2Int.up)
+        {
+            topWall.SetActive(false);
+            topDoor.SetActive(true);
+        }
 
-        if (direction == Vector2Int.down)bottonDoor.SetActive(true);
+        if (direction == Vector2Int.down)
+        {
+            bottonWall.SetActive(false);
+            bottonDoor.SetActive(true);
+        }
 
-        if(direction == Vector2Int.left) leftDoor.SetActive(true);
+        if (direction == Vector2Int.left)
+        {
+            leftWall.SetActive(false);
+            leftDoor.SetActive(true);
+        }
 
-        if(direction == Vector2Int.right)rightDoor.SetActive(true);
+        if(direction == Vector2Int.right)
+        {
+            rightWall.SetActive(false);
+            rightDoor.SetActive(true);
+        }
 
     }
 
