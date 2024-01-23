4cb7e57cb561b72ca30f8bae40d9fcb04092059b
angel [itb] navarrete sanchez <angel.navarrete.7e6@itb.cat>
2024-01-23 11:10:49 +0100

    R1.1-Entrega

--- HermenegilDOOM/Assets/Scripts/NPCDialogSystem.cs
+++ HermenegilDOOM/Assets/Scripts/NPCDialogSystem.cs
@@ -5,7 +5,6 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class NPCDialogSystem : MonoBehaviour
 {
